6d6e90075db85f8586d46a8b417409843adc0e72
minji-0801 <a50291383@gmail.com>
2025-03-04 15:53:57 +0900

    Update asgi.py

--- course-registration-vacancy-checker/config/config/asgi.py
+++ course-registration-vacancy-checker/config/config/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-에러발생
+에러발생 ㅇㅇㅇ
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
